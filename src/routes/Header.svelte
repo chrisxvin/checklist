56b0b901685483d1c0e4cc476d2be929dde1c041
Chris Liu <chris@ixvin.com>
2024-08-12 13:13:06 +0800

    feat: change logo color.

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,7 +5,7 @@ import github from "$lib/images/github.svg";
 
 <header class="flex justify-between">
     <div class="corner">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="text-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="logo">
             <title>Checklist</title>
             <path d="M3,5H9V11H3V5M5,7V9H7V7H5M11,7H21V9H11V7M11,15H21V17H11V15M5,20L1.5,16.5L2.91,15.09L5,17.17L9.59,12.59L11,14L5,20Z" />
         </svg>
@@ -71,14 +71,14 @@ nav {
     --background: rgba(255, 255, 255, 0.7);
 }
 
-svg {
+svg.logo {
     width: 2em;
     height: 3em;
     display: block;
 }
 
-path {
-    fill: var(--background);
+.logo path {
+    fill: var(--logo-color);
 }
 
 .links {
