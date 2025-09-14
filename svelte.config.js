8df33b8e69eb5ca8c68766e57c646589f8a7dc86
Chris Liu <chris@ixvin.com>
2025-09-14 13:22:41 +0800

    Upgrade packages.

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
         runes: true,
     },
 
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // Consult https://svelte.dev/docs/kit/integrations
     // for more information about preprocessors
     preprocess: vitePreprocess(),
 
@@ -18,6 +18,25 @@ const config = {
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({ out: "dist" }),
+        alias: {
+            "$types": "./src/types",
+            "$types/*": "./src/types/*",
+        },
+    },
+
+    vitePlugin: {
+        inspector: {
+            toggleKeyCombo: "alt-x",
+            showToggleButton: "always",
+            toggleButtonPos: "bottom-right",
+        },
+    },
+
+    onwarn: (warning, handler) => {
+        if (warning.code.startsWith("a11y_")) {
+            return;
+        }
+        handler(warning);
     },
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,59 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+// import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vitest/config";
+import rollupJscc from "rollup-plugin-jscc";
+import AutoImport from "unplugin-auto-import/vite";
+import devtoolsJson from "vite-plugin-devtools-json";
+
+const _DEBUG = process.env.NODE_ENV !== "production";
 
 export default defineConfig({
     server: {
         host: "0.0.0.0",
         port: 3000,
+        allowedHosts: true,
     },
-    plugins: [sveltekit()],
+    plugins: [
+        devtoolsJson(),
+        {
+            // 条件编译插件。作为 Vite 插件仅能在开发模式下使用。生产模式仍然要回归到 Rollup 插件。
+            ...rollupJscc({
+                values: {
+                    _DEBUG,
+                },
+            }),
+            apply: "serve",
+        },
+        // tailwindcss(),
+        sveltekit(),
+        AutoImport({
+            // include: [
+            //     /\.svelte$/,
+            //     /\.svelte\?svelte/, // .svelte
+            //     /\.md$/, // .md
+            // ],
+            imports: [
+                "svelte",
+                "svelte/store",
+                "svelte/transition",
+            ],
+            dirs: [
+                "./src/lib/components/**",
+            ],
+            dts: "./src/types/auto-imports.d.ts",
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            plugins: [
+                rollupJscc({
+                    values: {
+                        _DEBUG,
+                    },
+                }),
+            ],
+        },
+    },
     test: {
         include: ["src/**/*.{test,spec}.{js,ts}"],
     },
