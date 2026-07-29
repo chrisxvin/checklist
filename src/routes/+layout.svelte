b8207e94c6c909a14fb703b3958d67a1b570e2cf
Chris Liu <chris@ixvin.com>
2026-07-29 21:05:15 +0800

    简单做个样式。

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,14 @@ import "../app.css";
 <script lang="ts">
 import type { LayoutProps } from "./$types";
 
+import Header from "./Header.svelte";
+
 let { data, children }: LayoutProps = $props();
 </script>
 
 <div class="app mx-auto flex min-h-screen w-full flex-col self-center border border-blue-300 md:w-xl">
+    <Header {...data} />
+
     <!-- <slot /> -->
     {@render children()}
 </div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,139 +1,24 @@
-<script>
-import { page } from "$app/state";
-import github from "$lib/images/github.svg";
+<script lang="ts">
+import type { LayoutProps } from "./$types";
+
+let { logined, user }: LayoutProps["data"] = $props();
 </script>
 
-<header class="flex justify-between">
-    <div class="corner">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="logo">
-            <title>Checklist</title>
-            <path d="M3,5H9V11H3V5M5,7V9H7V7H5M11,7H21V9H11V7M11,15H21V17H11V15M5,20L1.5,16.5L2.91,15.09L5,17.17L9.59,12.59L11,14L5,20Z" />
-        </svg>
-    </div>
+<header class="flex items-center justify-between p-2">
+    <strong class="text-2xl">Preflight</strong>
+    <span>&nbsp;</span>
 
     <nav>
-        <svg viewBox="0 0 2 3" aria-hidden="true">
-            <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-        </svg>
-        <ul class="links">
-            <li aria-current={page.url.pathname === "/" ? "page" : undefined}>
-                <a href="/">Home</a>
-            </li>
-            <!--
-            <li aria-current={$page.url.pathname === "/about" ? "page" : undefined}>
-                <a href="/about">About</a>
-            </li>
-            <li aria-current={$page.url.pathname.startsWith("/sverdle") ? "page" : undefined}>
-                <a href="/sverdle">Sverdle</a>
-            </li>
-            -->
-        </ul>
-        <svg viewBox="0 0 2 3" aria-hidden="true">
-            <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-        </svg>
+        {#if logined && user}
+            <div class="avatar">
+                <div class="w-12 mask mask-squircle">
+                    <img src={user.picture} alt="Avatar" />
+                </div>
+            </div>
+        {:else}
+            <a class="link link-primary" href="/auth/login">Login</a>
+        {/if}
+
+        <ul class="links"></ul>
     </nav>
-
-    <div class="corner">
-        <!--
-        <a href="https://github.com/chrisxvin/checklist">
-            <img src={github} alt="GitHub" />
-        </a>
-         -->
-    </div>
 </header>
-
-<style lang="postcss">
-@reference "tailwindcss";
-
-.corner {
-    @apply size-12 flex justify-center;
-    /* width: 3em;
-    height: 3em;
-    display: flex;
-    justify-content: center; */
-}
-
-.corner a {
-    @apply flex items-center justify-center size-full;
-    /* display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%; */
-}
-
-.corner img {
-    @apply size-8 object-contain;
-    /* width: 2em;
-    height: 2em;
-    object-fit: contain; */
-}
-
-nav {
-    display: flex;
-    justify-content: center;
-    --background: #fafafa80;
-}
-
-nav path {
-    fill: var(--background);
-}
-
-svg {
-    width: 2em;
-    height: 3em;
-    display: block;
-}
-
-.logo path {
-    fill: var(--logo-color);
-}
-
-.links {
-    position: relative;
-    padding: 0;
-    margin: 0;
-    height: 3em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    list-style: none;
-    background: var(--background);
-    background-size: contain;
-}
-
-.links li {
-    position: relative;
-    height: 100%;
-}
-
-.links li[aria-current="page"]::before {
-    --size: 6px;
-    content: "";
-    width: 0;
-    height: 0;
-    position: absolute;
-    top: 0;
-    left: calc(50% - var(--size));
-    border: var(--size) solid transparent;
-    border-top: var(--size) solid var(--color-theme-1);
-}
-
-nav a {
-    display: flex;
-    height: 100%;
-    align-items: center;
-    padding: 0 0.5rem;
-    color: var(--color-text);
-    font-weight: 700;
-    font-size: 0.8rem;
-    text-transform: uppercase;
-    letter-spacing: 0.1em;
-    text-decoration: none;
-    transition: color 0.2s linear;
-}
-
-a:hover {
-    color: var(--color-theme-1);
-}
-</style>
